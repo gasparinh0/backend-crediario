dba80250f15c2d64391ba0302a1f5b1279512a44
Lucas Gaspari <lcsgaspari@gmail.com>
2025-01-07 20:02:10 -0300

    feat(clients controller): creating CRUD functions to clients

---
+++ src/controllers/clients.js
@@ -0,0 +1,80 @@
+const ClientModel = require('../models/clients.js')
+
+async function get(req, res) {
+    try {
+      const { id } = req.params;
+      const obj = id ? { _id: id } : {}; // Objeto de filtro, vazio para buscar todos
+  
+      const clients = await ClientModel.find(obj);
+      res.send(clients);
+    } catch (error) {
+      res.status(500).send({ message: 'Erro ao buscar clientes', error });
+    }
+  }
+  
+  async function post(req, res) {
+    try {
+      const { name, telephone } = req.body;
+  
+      if (!name || !telephone) {
+        return res.status(400).send({ message: 'Nome e telefone são obrigatórios.' });
+      }
+  
+      const client = new ClientModel({ name, telephone });
+      await client.save();
+  
+      res.send({ message: 'Cliente cadastrado com sucesso!' });
+    } catch (error) {
+      res.status(500).send({ message: 'Erro ao salvar cliente', error });
+    }
+  }
+
+  async function put(req, res) {
+    try {
+      const { id } = req.params; // Obtém o ID dos parâmetros da rota
+  
+      // Atualiza o cliente com base no ID
+      const client = await ClientModel.findOneAndUpdate(
+        { _id: id },       // Filtro para encontrar o cliente pelo ID
+        req.body,          // Dados de atualização (enviados no corpo da requisição)
+        { new: true }      // Retorna o documento atualizado
+      );
+  
+      // Caso o cliente não seja encontrado
+      if (!client) {
+        return res.status(404).send({ message: 'Cliente não encontrado' });
+      }
+  
+      // Responde com sucesso
+      res.send({
+        message: 'Success!',
+        client,
+      });
+    } catch (error) {
+      // Trata erros durante a operação
+      res.status(500).send({ message: 'Erro ao atualizar dados', error });
+    }
+  }
+  
+  async function remove(req, res) {
+    try {
+      const { id } = req.params
+      
+      const deletionResult = await ClientModel.deleteOne({ _id: id })
+  
+      const message = deletionResult.deletedCount = 1 ? 'success' : 'error'
+  
+      res.send({ 
+          message, 
+      })
+    } catch (error) {
+      res.status(500).send({ message: 'Erro ao apagar cliente', error })
+    }
+}
+  
+module.exports = {
+    get,
+    post,
+    put,
+    remove
+}
