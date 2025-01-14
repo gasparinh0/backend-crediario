25bb1337e06b6e800e19d51e38dc611021450fac
Lucas Gaspari <lcsgaspari@gmail.com>
2025-01-14 20:11:32 -0300

    feat(user model): adding name in the model

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Esquema do Usuário
 const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
