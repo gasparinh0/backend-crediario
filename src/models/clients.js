a6a9e9b2931633a6e407f0248185f9ad496f8f42
Lucas Gaspari <lcsgaspari@gmail.com>
2025-01-14 20:13:56 -0300

    feat(clients model): adding userId to the ClientSchema

--- src/models/clients.js
+++ src/models/clients.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ClientSchema = new mongoose.Schema({
   name: { type: String, required: true },
   telephone: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 module.exports = mongoose.model('clients', ClientSchema);
