6e8d08ae16ec86b1f838291046f6727a297416b4
Lucas Gaspari <lcsgaspari@gmail.com>
2025-02-18 16:19:47 -0300

    feat: adding cors to the project

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 //A boa prática é sempre chamar as dependencias que foram instaladas, depois as qual você criou no projeto.
 //dependencias instaladas
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 //importando env
 require('dotenv').config();
@@ -15,21 +15,20 @@ const app = express();
 //conexão com o banco de dados
 db.connect()
 
-// const allowedOrigins = [
-//    'http://127.0.0.1:5500',
-//    'http://www.app.com.br',
-// ]
-
-// //habilitando o cors
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         if (allowedOrigins.indexOf(origin) !== -1) {
-//           callback(null, true)
-//         } else {
-//           callback(new Error('Not allowed by CORS'))
-//         }
-//     }
-// }))
+const allowedOrigins = [
+    'http://localhost:5173',
+ ];
+ 
+ // Habilitando o CORS
+ app.use(cors({
+     origin: function (origin, callback) {
+         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+             callback(null, true);
+         } else {
+             callback(new Error('Not allowed by CORS'));
+         }
+     }
+ }));
 
 //habilita server para receber dados em formato json
 app.use(express.json())
